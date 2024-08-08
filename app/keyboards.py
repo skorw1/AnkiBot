cf76f2c9b603fc6ed3c6f8900277067e1f165a0e
kolya <so2bahila100@gmail.com>
2024-08-08 16:49:31 +0300

    Добавлен multiple_translation_menu и изменен start_menu

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,10 +1,22 @@
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton)
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-main = ReplyKeyboardMarkup(keyboard=[
+start_menu = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='Множественный перевод'), KeyboardButton(text='Мои темы')],
+        [KeyboardButton(text='/csv'), KeyboardButton(text='Моя статистика')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder='Воспользуйтесь меню ниже')
+
+main_menu = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='/csv')],
-],
+],  resize_keyboard=True,
+    input_field_placeholder='Воспользуйтесь меню ниже')
 
-                                                    resize_keyboard=True,
-                                                    input_field_placeholder='Воспользуйтесь меню ниже')
+multiple_translation_menu = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Раделитель: запятая')],
+    [KeyboardButton(text='Раделитель: пробел')],
+    [KeyboardButton(text='Раделитель: перенос строки')],
+    [KeyboardButton(text='Главное меню')],
+])
 
 
