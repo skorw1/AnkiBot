6b758cd0588424e84fa1987f6dec3a82095e788c
kolya <so2bahila100@gmail.com>
2024-08-09 15:08:58 +0300

    Добавлена клавиатура для множественного перевода

--- app/keyboards.py
+++ app/keyboards.py
@@ -13,9 +13,6 @@ main_menu = ReplyKeyboardMarkup(keyboard=[
     input_field_placeholder='Воспользуйтесь меню ниже')
 
 multiple_translation_menu = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Раделитель: запятая')],
-    [KeyboardButton(text='Раделитель: пробел')],
-    [KeyboardButton(text='Раделитель: перенос строки')],
     [KeyboardButton(text='Главное меню')],
 ])
 
