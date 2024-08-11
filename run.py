8c56759d704db0fa2e7f4c294a6a744c2217d9ea
kolya <so2bahila100@gmail.com>
2024-08-11 18:01:06 +0300

    Добавлена функция для запуска БД

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from app.handlers import router
+from app.database import create_tables
 
 # Настройка логгирования
 logging.basicConfig(level=logging.INFO)
@@ -11,11 +12,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dispatcher = Dispatcher()
 
+async def on_startup_func():
+    print("Запуск on_startup...")
+    await create_tables()
+    logging.info('База данных успешно настроена')
+
 async def main() -> None:
     dispatcher.include_router(router)
     
     logging.info("Бот запускается...")
-    await dispatcher.start_polling(bot)
+    await on_startup_func()
+    await dispatcher.start_polling(bot, on_startup=on_startup_func)
 
 if __name__ == '__main__':
     try:
